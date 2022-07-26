7eab4d2d7c4d3f47788d6b2a12556ae7dab58097
unknown <Alamin>
2022-07-26 18:28:30 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
+import Navbar from "./pages/SharedPages/Navbar";
 
 function App() {
   return (
-    <div className="App">
+    <div className="text-blue-500">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
