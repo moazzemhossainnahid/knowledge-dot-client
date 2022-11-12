61cd20f6b9005405207f1a97e50e89a2080ae8ba
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2022-11-12 20:31:35 +0600

    tailwind config add

--- tailwind.config.js
+++ tailwind.config.js
@@ -90,26 +90,3 @@ module.exports = {
     ],
   },
 };
-// module.exports = ({
-//   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
-//   theme: {
-//     screens: {
-//       sm: "320px",
-//       // => @media (min-width: 640px) { ... }
-
-//       md: "768px",
-//       // => @media (min-width: 768px) { ... }
-
-//       lg: "1024px",
-//       // => @media (min-width: 1024px) { ... }
-
-//       xl: "1280px",
-//       // => @media (min-width: 1280px) { ... }
-
-//       "2xl": "1536px",
-//       // => @media (min-width: 1536px) { ... }
-//     },
-//     extend: {},
-//   },
-//   plugins: [require("daisyui"), require('tailwind-scrollbar-hide')],
-// });
