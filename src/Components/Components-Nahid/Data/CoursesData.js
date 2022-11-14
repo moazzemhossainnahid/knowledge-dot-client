21c26c8721f17f28106a04de9787d03ea460aa6d
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2022-11-14 22:24:52 +0600

    add courses new data

--- src/Components/Components-Nahid/Data/CoursesData.js
+++ src/Components/Components-Nahid/Data/CoursesData.js
@@ -11,7 +11,7 @@ export const AllCoursesData = [
         "instructorimg": "https://github.githubassets.com/images/modules/profile/achievements/yolo-default.png",
         "thumbimg": "https://static.vecteezy.com/system/resources/thumbnails/003/266/872/small/career-business-work-concept-with-business-goals-team-free-vector.jpg",
         "introvdo": "https://www.youtube.com/embed/RhRJh4oCySU",
-        "list": "development",
+        "list": "Development",
         "category": "Web Development",
         "rating": {
             "total_rating": 20,
@@ -20,7 +20,7 @@ export const AllCoursesData = [
         "details": {
             "description": "See-through delicate embroidered organza blue lining luxury acetate-mix stretch pleat detailing. Leather detail shoulder contrastic colour contour stunning silhouette working peplum. Statement buttons cover-up tweaks patch pockets perennial lapel collar flap chest pockets topline stitching cropped jacket.",
             "certificate_details": "Effortless comfortable full leather lining eye-catching unique detail to the toe low ‘cut-away’ sides clean and sleek. Polished finish elegant court shoe work duty stretchy slingback strap mid kitten heel this ladylike design slingback strap mid kitten heel this ladylike design.",
-            "for_whom": "Anyone interested in learning about business (only practical concepts that you can use and no boring theory + we won’t cover business topics that are common sense).",
+            "for_whom": "Anyone interested in learning about business (only practical concepts that you can use and no boring theory + we won’t cover business topics that are common sense)."
         },
         "files": [
             {
@@ -59,7 +59,196 @@ export const AllCoursesData = [
                 ]
             }
         ],
-        "price": 6500,
+        "price": 6500
+    },
+    {
+        "uname": "php-laravel-development",
+        "name": "PHP Laravel Development",
+        "level": "Beginner",
+        "badge": "Top",
+        "duration": 15,
+        "language": "Hindi",
+        "accessibility": "Lifetime",
+        "instructor": "Abdur Rahman",
+        "instructorimg": "https://i.ibb.co/FKXS8kM/Screenshot-13.png",
+        "thumbimg": "https://programmerhope.com/wp-content/uploads/2019/03/laravel-devepomnt.png",
+        "introvdo": "https://www.youtube.com/watch?v=Hqd8dZrw5bg",
+        "list": "Development",
+        "category": "Web Development",
+        "rating": {
+            "total_rating": 27,
+            "total_people": 6
+        },
+        "details": {
+            "description": "See-through delicate embroidered organza blue lining luxury acetate-mix stretch pleat detailing. Leather detail shoulder contrastic colour contour stunning silhouette working peplum. Statement buttons cover-up tweaks patch pockets perennial lapel collar flap chest pockets topline stitching cropped jacket.",
+            "certificate_details": "Effortless comfortable full leather lining eye-catching unique detail to the toe low ‘cut-away’ sides clean and sleek. Polished finish elegant court shoe work duty stretchy slingback strap mid kitten heel this ladylike design slingback strap mid kitten heel this ladylike design.",
+            "for_whom": "Anyone interested in learning about business (only practical concepts that you can use and no boring theory + we won’t cover business topics that are common sense)."
+        },
+        "files": [
+            {
+                "title": "Lesson 01",
+                "details": [
+                    {
+                        "name": "Intro to PHP Laravel Development",
+                        "fileName": "Intro-to-PHP-laravel-Development",
+                        "vurl": "https://www.youtube.com/embed/DRbhTPEDRbA"
+                    },
+                    {
+                        "name": "Overview Of PHP Development",
+                        "fileName": "Overview-Of-php-Development",
+                        "vurl": "https://www.youtube.com/embed/DRbhTPEDRbA"
+                    }
+                ]
+            },
+            {
+                "title": "Lesson 02",
+                "details": [
+                    {
+                        "name": "PHP Job Overview",
+                        "fileName": "php-Job-Overview",
+                        "vurl": "https://www.youtube.com/embed/DRbhTPEDRbA"
+                    },
+                    {
+                        "name": "Identifying PHP Skills",
+                        "fileName": "Identifying-php-Skills",
+                        "vurl": "https://www.youtube.com/embed/DRbhTPEDRbA"
+                    },
+                    {
+                        "name": "Laravel Focus Quiz",
+                        "fileName": "laravel-focus-quiz",
+                        "vurl": "https://www.youtube.com/embed/DRbhTPEDRbA"
+                    }
+                ]
+            },
+            {
+                "title": "Lesson 03",
+                "details": [
+                    {
+                        "name": "Learn to build applications using laravel",
+                        "fileName": "learn-to-build-applications-using-laravel",
+                        "vurl": "https://www.youtube.com/embed/DRbhTPEDRbA"
+                    },
+                    {
+                        "name": "To install Laravel using Windows and MAC",
+                        "fileName": "to-install-laravel-using-windows-and-mac",
+                        "vurl": "https://www.youtube.com/embed/DRbhTPEDRbA"
+                    },
+                    {
+                        "name": "You will learn how to use routes",
+                        "fileName": "You-will-learn-how-to-use-routes",
+                        "vurl": "https://www.youtube.com/embed/DRbhTPEDRbA"
+                    }
+                ]
+            }
+        ],
+        "price": 5700
+    },
+    {
+        "uname": "NodeJS-The-Complete-Guide",
+        "name": "NodeJS - The Complete Guide (MVC, REST APIs, GraphQL, Deno)",
+        "level": "Intermediate",
+        "badge": "Popular",
+        "duration": 37,
+        "language": "English",
+        "accessibility": "Lifetime",
+        "instructor": "MD. NAHID",
+        "instructorimg": "https://i.ibb.co/dMgYbwW/nahid.jpg",
+        "thumbimg": "https://www.freecodecamp.org/news/content/images/2021/04/nodeexpress.png",
+        "introvdo": "https://www.youtube.com/watch?v=8u1o-OmOeGQ",
+        "list": "Development",
+        "category": "Web Development",
+        "rating": {
+            "total_rating": 49,
+            "total_people": 10
+        },
+        "details": {
+            "description": "See-through delicate embroidered organza blue lining luxury acetate-mix stretch pleat detailing. Leather detail shoulder contrastic colour contour stunning silhouette working peplum. Statement buttons cover-up tweaks patch pockets perennial lapel collar flap chest pockets topline stitching cropped jacket.",
+            "certificate_details": "Effortless comfortable full leather lining eye-catching unique detail to the toe low ‘cut-away’ sides clean and sleek. Polished finish elegant court shoe work duty stretchy slingback strap mid kitten heel this ladylike design slingback strap mid kitten heel this ladylike design.",
+            "for_whom": "Anyone interested in learning about business (only practical concepts that you can use and no boring theory + we won’t cover business topics that are common sense)."
+        },
+        "files": [
+            {
+                "title": "Lesson 01",
+                "details": [
+                    {
+                        "name": "Node.js Crash Course Tutorial #1 - Introduction & Setup",
+                        "fileName": "Introduction-and-Setup",
+                        "vurl": "https://www.youtube.com/watch?v=zb3Qk8SG5Ms&list=PL4cUxeGkcC9jsz4LDYc6kv3ymONOKxwBU"
+                    },
+                    {
+                        "name": "Node.js Crash Course Tutorial #2 - Node.js Basics",
+                        "fileName": "Node.js-Basics",
+                        "vurl": "https://www.youtube.com/watch?v=OIBIXYLJjsI&list=PL4cUxeGkcC9jsz4LDYc6kv3ymONOKxwBU&index=2"
+                    }
+                ]
+            },
+            {
+                "title": "Lesson 02",
+                "details": [
+                    {
+                        "name": "Node.js Crash Course Tutorial #3 - Clients & Servers",
+                        "fileName": "Clients-and-Servers",
+                        "vurl": "https://www.youtube.com/watch?v=-HPZ1leCV8k&list=PL4cUxeGkcC9jsz4LDYc6kv3ymONOKxwBU&index=3"
+                    },
+                    {
+                        "name": "Node.js Crash Course Tutorial #4 - Requests & Responses",
+                        "fileName": "Requests-and-Responses",
+                        "vurl": "https://www.youtube.com/watch?v=DQD00NAUPNk&list=PL4cUxeGkcC9jsz4LDYc6kv3ymONOKxwBU&index=4"
+                    },
+                    {
+                        "name": "Node.js Crash Course Tutorial #5 - NPM",
+                        "fileName": "npm",
+                        "vurl": "https://www.youtube.com/watch?v=bdHE2wHT-gQ&list=PL4cUxeGkcC9jsz4LDYc6kv3ymONOKxwBU&index=5"
+                    }
+                ]
+            },
+            {
+                "title": "Lesson 03",
+                "details": [
+                    {
+                        "name": "Node.js Crash Course Tutorial #6 - Express Apps",
+                        "fileName": "learn-express",
+                        "vurl": "https://www.youtube.com/watch?v=Lr9WUkeYSA8&list=PL4cUxeGkcC9jsz4LDYc6kv3ymONOKxwBU&index=6"
+                    },
+                    {
+                        "name": "Node.js Crash Course Tutorial #7 - View Engines",
+                        "fileName": "view-engine",
+                        "vurl": "https://www.youtube.com/watch?v=yXEesONd_54&list=PL4cUxeGkcC9jsz4LDYc6kv3ymONOKxwBU&index=7"
+                    },
+                    {
+                        "name": "Node.js Crash Course Tutorial #8 - Middleware",
+                        "fileName": "middlewares",
+                        "vurl": "https://www.youtube.com/watch?v=_GJKAs7A0_4&list=PL4cUxeGkcC9jsz4LDYc6kv3ymONOKxwBU&index=8"
+                    },
+                    {
+                        "name": "Node.js Crash Course Tutorial #9 - MongoDB",
+                        "fileName": "learn-mongodb",
+                        "vurl": "https://www.youtube.com/watch?v=bxsemcrY4gQ&list=PL4cUxeGkcC9jsz4LDYc6kv3ymONOKxwBU&index=9"
+                    }
+                ]
+            },
+            {
+                "title": "Lesson 04",
+                "details": [
+                    {
+                        "name": "Node.js Crash Course Tutorial #10 - Get, Post & Delete Requests",
+                        "fileName": "learn-crud-operations",
+                        "vurl": "https://www.youtube.com/watch?v=VVGgacjzc2Y&list=PL4cUxeGkcC9jsz4LDYc6kv3ymONOKxwBU&index=10"
+                    },
+                    {
+                        "name": "Node.js Crash Course Tutorial #11 - Express Router & MVC",
+                        "fileName": "router-and-mvc",
+                        "vurl": "https://www.youtube.com/watch?v=zW_tZR0Ir3Q&list=PL4cUxeGkcC9jsz4LDYc6kv3ymONOKxwBU&index=11"
+                    },
+                    {
+                        "name": "Node.js Crash Course Tutorial #12 - Wrap up",
+                        "fileName": "wrap-up",
+                        "vurl": "https://www.youtube.com/watch?v=nYAyhRAV87A&list=PL4cUxeGkcC9jsz4LDYc6kv3ymONOKxwBU&index=12"
+                    }
+                ]
+            }
+        ],
+        "price": 7500
     },
 
 ]
