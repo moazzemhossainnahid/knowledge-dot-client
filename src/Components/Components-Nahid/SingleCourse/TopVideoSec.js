338a7c65fa3888c7923829a8bdb7ac7d002a9a0f
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2022-11-16 22:06:56 +0600

    video container added

--- src/Components/Components-Nahid/SingleCourse/TopVideoSec.js
+++ src/Components/Components-Nahid/SingleCourse/TopVideoSec.js
@@ -6,7 +6,7 @@ import { faAccessibleIcon } from '@fortawesome/free-brands-svg-icons';
 
 
 const TopVideoSec = ({ course }) => {
-    console.log(course); 
+    // console.log(course); 
     // console.log(course?.files.length);
 
     return (
@@ -15,7 +15,7 @@ const TopVideoSec = ({ course }) => {
             <div className="flex flex-col lg:flex-row justify-center items-center gap-5 w-full h-full p-5">
                 <div className="w-full lg:w-4/6 mx-auto">
                     <div className="lg:h-[450px] h-56 md:h-96 w-full rounded-xl">
-                        {course?.introvdo ? (
+                        {course?.introvd ? (
                             <ReactPlayer
                                 width={"100%"}
                                 height={"100%"}
