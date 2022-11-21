cdc4b80b6316a00a5883496110763a9a9150661b
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2022-11-21 22:16:14 +0600

    dynamic populer courses add

--- src/Components/Components-Nahid/HomePageSection/PopulerCourses/PopulerCourses.js
+++ src/Components/Components-Nahid/HomePageSection/PopulerCourses/PopulerCourses.js
@@ -1,7 +1,16 @@
+import { faBook, faInr, faUser } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
-import { PopulerCoursesData } from '../../Data/PopulerCoursesData';
+import { useNavigate } from 'react-router-dom';
+import useCourses from '../../../../Apis/useCourses';
 
 const PopulerCourses = () => {
+    const {Courses} = useCourses();
+    const navigate = useNavigate();
+
+    const Populer = Courses?.filter(course => course?.rating?.total_rating / course?.rating?.total_people > 3.50);
+
+    console.log(Populer);
     return (
         <div>
             <div className='py-10'>
@@ -18,29 +27,99 @@ const PopulerCourses = () => {
                     <div className="w-full md:w-4/6 mx-auto">
                         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 p-5">
                             {
-                                PopulerCoursesData.map((data, idx) => {
+                                Populer?.map((course) => {
+                                    const ratings = Math.ceil(course?.rating?.total_rating / course?.rating?.total_people);
                                     return (
-                                        <div key={idx} class="card rounded hover:rounded-md mx-5 group w-full bg-base-100 overflow-hidden shadow-xl hover:text-white hover:bg-gradient-to-tr from-[#090909bb] to-[#0000008f] bg-cover bg-center duration-300">
-                                            <div class="card-actions justify-end absolute top-5 right-2">
-                                                <div class="badge bg-blue-500 px-3 py-2">${data?.price}</div>
-                                            </div>
-                                            <figure><img className='group-hover:scale-[4] group-hover:mix-blend-overlay  duration-300' src={data?.img} alt="Shoes" /></figure>
-                                            <div class="card-body bg-cover bg-center z-10">
-                                                <div className="w-full mx-auto -mt-16 p-3 group-hover:bg-[#18181862] bg-base-100 shadow-lg">
-                                                    <h2 class="card-title">{data?.title}</h2>
-                                                    <p className='flex items-center justify-center gap-2'>
+                                        <div key={course?._id} onClick={() => navigate(`/course/${course?._id}`)} class="card cursor-pointer group w-full overflow-hidden bg-base-100 shadow-xl hover:text-white hover:bg-gradient-to-tr from-[#090909bb] to-[#0000008f] bg-cover bg-center duration-300">
+                                        <div class="card-actions justify-end absolute top-5 right-2">
+                                            <div class="badge badge-primary px-3 py-2"><FontAwesomeIcon icon={faInr} /> {course?.price} </div>
+                                        </div>
+                                        <figure><img className='group-hover:scale-[4] group-hover:mix-blend-overlay  duration-300' src={course?.thumbimg} alt="Shoes" /></figure>
+                                        <div class="card-body bg-cover bg-center z-10">
+                                            <div className="w-full rounded mx-auto -mt-16 p-3 group-hover:bg-[#18181862] bg-base-100 shadow-lg">
+                                                <h2 class="text-md font-bold">{course?.name?.slice(0, 30)}</h2>
+                                                <p className='flex items-center justify-between gap-2'>
+                                                    <div className="flex items-center gap-2">
+                                                        <span className="text-md font-bold text-gray-600"> {ratings} </span>
                                                         <div class="rating">
-                                                            <input type="radio" name="rating-2" class="mask w-3 h-3 mask-star-2 bg-orange-400" />
-                                                            <input type="radio" name="rating-2" class="mask w-3 h-3 mask-star-2 bg-orange-400" checked />
-                                                            <input type="radio" name="rating-2" class="mask w-3 h-3 mask-star-2 bg-orange-400" />
-                                                            <input type="radio" name="rating-2" class="mask w-3 h-3 mask-star-2 bg-orange-400" />
-                                                            <input type="radio" name="rating-2" class="mask w-3 h-3 mask-star-2 bg-orange-400" />
+                                                            {
+                            
+                                                                ratings === 1 &&
+                                                                <div className="rating">
+                                                                    <input name="rating-1" className="mask mask-star w-4 md:w-4  bg-green-500" checked />
+                                                                    <input name="rating-1" className="mask mask-star w-4 md:w-4  bg-gray-300" />
+                                                                    <input name="rating-1" className="mask mask-star w-4 md:w-4  bg-gray-300" />
+                                                                    <input name="rating-1" className="mask mask-star w-4 md:w-4  bg-gray-300" />
+                                                                    <input name="rating-1" className="mask mask-star w-4 md:w-4  bg-gray-300" />
+                                                                </div>
+                            
+                                                            }
+                                                            {
+                            
+                                                                ratings === 2 &&
+                                                                <div className="rating">
+                                                                    <input name="rating-1" className="mask mask-star w-4 md:w-4  bg-green-500" />
+                                                                    <input name="rating-1" className="mask mask-star w-4 md:w-4  bg-green-500" checked />
+                                                                    <input name="rating-1" className="mask mask-star w-4 md:w-4  bg-gray-300" />
+                                                                    <input name="rating-1" className="mask mask-star w-4 md:w-4  bg-gray-300" />
+                                                                    <input name="rating-1" className="mask mask-star w-4 md:w-4  bg-gray-300" />
+                                                                </div>
+                            
+                                                            }
+                                                            {
+                            
+                                                                ratings === 3 &&
+                                                                <div className="rating">
+                                                                    <input name="rating-1" className="mask mask-star w-4 md:w-4  bg-green-500" />
+                                                                    <input name="rating-1" className="mask mask-star w-4 md:w-4  bg-green-500" />
+                                                                    <input name="rating-1" className="mask mask-star w-4 md:w-4  bg-green-500" checked />
+                                                                    <input name="rating-1" className="mask mask-star w-4 md:w-4  bg-gray-300" />
+                                                                    <input name="rating-1" className="mask mask-star w-4 md:w-4  bg-gray-300" />
+                                                                </div>
+                            
+                                                            }
+                                                            {
+                            
+                                                                ratings === 4 &&
+                                                                <div className="rating">
+                                                                    <input name="rating-1" className="mask mask-star w-4 md:w-4  bg-green-500" />
+                                                                    <input name="rating-1" className="mask mask-star w-4 md:w-4  bg-green-500" />
+                                                                    <input name="rating-1" className="mask mask-star w-4 md:w-4  bg-green-500" />
+                                                                    <input name="rating-1" className="mask mask-star w-4 md:w-4  bg-green-500" checked />
+                                                                    <input name="rating-1" className="mask mask-star w-4 md:w-4  bg-gray-300" />
+                                                                </div>
+                            
+                                                            }
+                                                            {
+                            
+                                                                ratings === 5 &&
+                                                                <div className="rating">
+                                                                    <input name="rating-1" className="mask mask-star w-4 md:w-4  bg-green-500" />
+                                                                    <input name="rating-1" className="mask mask-star w-4 md:w-4  bg-green-500" />
+                                                                    <input name="rating-1" className="mask mask-star w-4 md:w-4  bg-green-500" />
+                                                                    <input name="rating-1" className="mask mask-star w-4 md:w-4  bg-green-500" />
+                                                                    <input name="rating-1" className="mask mask-star w-4 md:w-4  bg-green-500" checked />
+                                                                </div>
+                            
+                                                            }
                                                         </div>
-                                                        <span className="text-sm"> 4.0 (1 rating) </span>
-                                                    </p>
+                                                    </div>
+                            
+                                                    <span className="text-xs flex"> ({course?.rating?.total_people} Rating) </span>
+                                                </p>
+                                            </div>
+                                            <div class="card-actions flex-col mx-auto gap-3 justify-center py-7">
+                                                <div className="flex items-center justify-center gap-3 group-hover:text-white">
+                                                    <FontAwesomeIcon icon={faBook} />
+                                                    <h3 className="text-md">{course?.files?.length} Lessons</h3>
+                                                </div>
+                                                <div className="flex items-center justify-center gap-3 group-hover:text-white">
+                                                    <FontAwesomeIcon icon={faUser} />
+                                                    <h3 className="text-md">23 Students</h3>
                                                 </div>
                                             </div>
                                         </div>
+                                    </div>
                                     )
                                 })
                             }
