5427703bbcf1ffa98f6fcf358581d9205d656ef7
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-02-10 21:29:49 +0600

    my course component add

---
+++ src/Pages/MyCourse/MyCourse.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const MyCourse = () => {
+
+    return (
+        <div className='w-full h-screen text-center mx-auto flex justify-center items-center'>
+           
+
+        </div>
+    );
+};
+
+export default MyCourse;
